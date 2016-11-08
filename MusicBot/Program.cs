4c64fcc01d699a78fa2fcfae94655481e5cb466a
Noahkiq <noahkiqgaming@gmail.com>
2016-11-08 11:42:04 -0500

    Create m!summon

--- MusicBot/Program.cs
+++ MusicBot/Program.cs
@@ -3,13 +3,61 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Discord;
+using Discord.Audio;
+using System.IO;
+using System.Timers;
+using NAudio;
+using NAudio.Wave;
+using NAudio.CoreAudioApi;
 
-namespace MusicBot
+class Program
 {
-    class Program
+    static void Main(string[] args) => new Program().Start();
+
+    public static string botPrefix = "m!";
+
+    private DiscordClient _client;
+
+    public void Start()
     {
-        static void Main(string[] args)
+        _client = new DiscordClient();
+
+        _client.UsingAudio(x => // Opens an AudioConfigBuilder so we can configure our AudioService
         {
-        }
+            x.Mode = AudioMode.Outgoing; // Tells the AudioService that we will only be sending audio
+        });
+
+        _client.Log.Message += (s, e) => Console.WriteLine($"[{e.Severity}] {e.Source}: {e.Message}");
+
+
+        _client.MessageReceived += async (s, e) =>
+        {
+            if ((e.Channel.Name.Contains("music")) && (e.Message.Text.StartsWith(botPrefix)))
+            {
+                if (e.Message.Text.StartsWith($"{botPrefix}summon"))
+                {
+                    if (!string.IsNullOrWhiteSpace(e.User.VoiceChannel.Name))
+                    {
+                        string userVC = e.User.VoiceChannel.Name;
+                        var voiceChannel = _client.FindServers(e.Server.Name).FirstOrDefault().FindChannels(userVC).FirstOrDefault();
+
+                        var _vClient = await _client.GetService<AudioService>() // We use GetService to find the AudioService that we installed earlier. In previous versions, this was equivelent to _client.Audio()
+                                .Join(voiceChannel); // Join the Voice Channel, and return the IAudioClient.
+                    }
+                    else
+                    {
+                        await e.Channel.SendMessage($"You must be in a voice channel to use this command!");
+                    }
+                }
+            }
+        };
+
+        string token = File.ReadAllText("token.txt");
+        _client.ExecuteAndWait(async () => {
+            await _client.Connect(token, TokenType.Bot);
+            _client.SetGame("some music");
+        });
+
     }
 }
